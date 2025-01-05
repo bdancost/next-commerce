e8bff6c421f6e57db0baf6b4300143e8fba368d9
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-01-05 00:29:41 -0300

    Adicionando Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar />
+        <main className="bg-slate-700 h-screen p-16">{children}</main>
       </body>
     </html>
   );
