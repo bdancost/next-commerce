aad3b4d0dd12373770441f7c356aa5f86369f110
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-01-05 01:06:36 -0300

    Definindo pagina para produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-export default function Home() {
+import { ProductType } from "@/types/ProductType";
+
+async function getProducts() {
+  const response = await fetch("https://fakestoreapi.com/products");
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return response.json();
+}
+
+export default async function Home() {
+  const products = await getProducts();
+  console.log(products);
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        <div className="bg-white rounded-lg shadow-md p-6">Prod 1</div>
+        {products.map((product: ProductType) => (
+          <div key={product.id}>{product.title}</div>
+        ))}
       </div>
-      <h1>Next E-Commerce</h1>
     </div>
   );
 }
