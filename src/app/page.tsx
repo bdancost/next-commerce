9d0a73c5ce340da84ced8b12beef24d93fdc0076
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-01-05 00:46:18 -0300

    Listagem de produtos parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        <div className="bg-white rounded-lg shadow-md p-6">Prod 1</div>
+      </div>
       <h1>Next E-Commerce</h1>
     </div>
   );
