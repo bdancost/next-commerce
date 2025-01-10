e2c3b0c6643ccf3930c1c5b36cee9ce03adeb35b
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-01-09 22:36:20 -0300

    adicionando webhooks

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
+// Cria uma instância global do Prisma no ambiente de desenvolvimento para evitar múltiplas conexões
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "development") {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+} else {
+  prisma = new PrismaClient();
 }
-
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "development") global.prisma = prisma;
 
 export default prisma;
